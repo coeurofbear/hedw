920e8a1198ece79d0ebda69025b2cfa2a6ac51b7
Héctor Palma Téllez <hector@palmatellez.cl>
2015-01-25 01:08:47 -0300

    Velocidad de scroll y flechita del primer slide avanza al segundo con nombre correcto.

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,9 @@ $(function() {
     $('.linea-de-tiempo').fullpage({
         resize: false,
         anchors:['inicio', 'el-comienzo', 'diseno-tablas', 'nuevas-tecnologias', 'ascenso-flash-javascript', 'web2-0', 'diseno-responsivo'],
-        navigation: true
+        navigation: true,
+        scrollingSpeed: 450
     });
-    // $.localScroll({
-    //     duration: 300
-    // });
     $('.js-menu').click(function(){
         $(this).toggleClass('activo');
         $('.menu-desplegable').slideToggle();
