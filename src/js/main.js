a3644fbee5a88d3d0ac4c30200d29abf310fb722
Héctor Palma Téllez <hector@palmatellez.cl>
2015-01-24 23:02:42 -0300

    Primer borrador de menú.

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,12 @@ $(function() {
         anchors:['slide1', 'slide2', 'slide3', 'slide4'],
         navigation: true
     });
-    $.localScroll({
-        duration: 300
+    // $.localScroll({
+    //     duration: 300
+    // });
+    $('.js-menu').click(function(){
+        $(this).toggleClass('activo');
+        $('.menu-desplegable').slideToggle();
+        $('#fp-nav').toggleClass('hide');
     });
 });
